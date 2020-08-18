d60784c6a8426360d5cf266e82da1eb45ce87733
Adi Nugroho <addieze@ymail.com>
2020-08-18 11:50:33 +0700

    add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,25 @@ import React from 'react'
 import {
     Link
 } from 'react-router-dom'
+
+import CasinoIcon from '@material-ui/icons/Casino';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import '../styles/tailwind.css';
 
 function Header() {
+
+    const toggleMenu = () => {
+        var element = document.getElementById("myDIV");
+        element.classList.toggle("hidden");
+    }
+
     return (
         <div
-            className="flex items-center w-full h-20 justify-between flex-wrap bg-myColor-second p-6 fixed"
+            className="flex items-center w-full justify-between flex-wrap bg-myColor-second p-6 fixed"
         >
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <img width="54" src="/img/lime-white.svg" alt="" />
+                <CasinoIcon fontSize="large" />
 
                 <span className="font-semibold text-xl tracking-tight pl-4 text-gray-100">
                     <Link to="/">Peluang Ku</Link>
@@ -19,23 +29,17 @@ function Header() {
             <div className="block lg:hidden">
                 <button
                     id="drop-button"
-                    className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+                    className="btn-menu"
+                    style={{}}
                 >
-                    <svg
-                        className="fill-current h-3 w-3"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <title>Menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                    </svg>
+                    <MenuIcon onClick={toggleMenu} />
                 </button>
             </div>
             <div
-                id="dropdown"
+                id="myDIV"
                 className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden"
             >
-                <div className="text-md lg:flex-grow text-gray-100">
+                <div className="text-md lg:flex-grow text-gray-100 ">
                     <Link
                         to="/panduan"
                         className="block mt-4 lg:inline-block lg:mt-0 mr-4"
