fefe32d97de9408d55ce3814e9be328eb84b3014
Adi Nugroho <addieze@ymail.com>
2020-08-22 15:39:43 +0700

    Updates

---
+++ precache-manifest.1fa5104500c8c8bb6e39c88811c6dce2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7207dedc4dca2d5089258720bc96397f",
+    "url": "/peluangku/index.html"
+  },
+  {
+    "revision": "70d96a1871c75bece653",
+    "url": "/peluangku/static/css/main.9a1324a3.chunk.css"
+  },
+  {
+    "revision": "1e19138ef5d55d26db63",
+    "url": "/peluangku/static/js/2.08e2a9c3.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/peluangku/static/js/2.08e2a9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d96a1871c75bece653",
+    "url": "/peluangku/static/js/main.c7e868b5.chunk.js"
+  },
+  {
+    "revision": "61f4f265cb656cfdc90a",
+    "url": "/peluangku/static/js/runtime-main.3ccd342e.js"
+  },
+  {
+    "revision": "4cde6d514b8e8d1ef05344718ba90b25",
+    "url": "/peluangku/static/media/book_lover.4cde6d51.svg"
+  },
+  {
+    "revision": "4905b169a01a1b6f16382b93c130770c",
+    "url": "/peluangku/static/media/celebration.4905b169.svg"
+  },
+  {
+    "revision": "947be6f71644020078a3670c9a1040f1",
+    "url": "/peluangku/static/media/dadu.947be6f7.jpg"
+  },
+  {
+    "revision": "ab7a83bb3f56cf9a6fafd95f72f5c80e",
+    "url": "/peluangku/static/media/feeling_blue.ab7a83bb.svg"
+  },
+  {
+    "revision": "9baf8b2d7cd6d75ea7ed9297805cf3d9",
+    "url": "/peluangku/static/media/fermat-&-pascal.9baf8b2d.jpg"
+  },
+  {
+    "revision": "3be6ea24ff38f9bae41e423fe2d96ef0",
+    "url": "/peluangku/static/media/joyride.3be6ea24.svg"
+  },
+  {
+    "revision": "a69b75d44725528a3df1685e080f80ec",
+    "url": "/peluangku/static/media/koin.a69b75d4.jpg"
+  },
+  {
+    "revision": "d6101f839e41ed45604da488e1f8c421",
+    "url": "/peluangku/static/media/page_not_found.d6101f83.svg"
+  },
+  {
+    "revision": "a9fdf067c5428c37786c2e5c09d705c5",
+    "url": "/peluangku/static/media/paper-test.a9fdf067.jpg"
+  },
+  {
+    "revision": "ae23317525f6e951603b82f785bcea36",
+    "url": "/peluangku/static/media/questions.ae233175.svg"
+  },
+  {
+    "revision": "52473eb158d04200667a6864fdcdc67a",
+    "url": "/peluangku/static/media/road_sign.52473eb1.svg"
+  },
+  {
+    "revision": "02eed3b84940a36c95f55fcd9be682f6",
+    "url": "/peluangku/static/media/to_do_list.02eed3b8.svg"
+  },
+  {
+    "revision": "cc2fc7a59f8088479e8ff8d900819883",
+    "url": "/peluangku/static/media/wasit.cc2fc7a5.jpg"
+  }
+]);
